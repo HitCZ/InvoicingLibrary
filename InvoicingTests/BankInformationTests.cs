3fd892389c58732f707e47079bb5725ee2523dfd
Lukáš Bubník <lukas.bu@seznam.cz>
2020-02-01 20:43:16 +0100

    Initial commit

---
+++ InvoicingTests/BankInformationTests.cs
@@ -0,0 +1,60 @@
+﻿using Invoicing.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace InvoicingTests
+{
+    [TestClass]
+    public class BankInformationTests
+    {
+        private const string SIMPLE_ACCOUNT_NUMBER = "1234567";
+        private const string BANK_CODE = "0800";
+        private readonly string accountNumber = $"{SIMPLE_ACCOUNT_NUMBER}/{BANK_CODE}";
+
+        [TestMethod]
+        public void GetSimpleAccountNumberTest()
+        {
+            var testConfigs = new List<(string accountNumber, string expectedResult)>
+            {
+                (null, string.Empty),
+                (string.Empty, string.Empty),
+                (SIMPLE_ACCOUNT_NUMBER, string.Empty),
+                (accountNumber, SIMPLE_ACCOUNT_NUMBER)
+            };
+
+            foreach (var config in testConfigs)
+            {
+                var accNumber = config.accountNumber;
+                var expected = config.expectedResult;
+                var information = new BankInformation { AccountNumber = accNumber };
+
+                var actual = information.GetSimpleAccountNumber();
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        public void GetBankCodeTest()
+        {
+            var testConfigs = new List<(string accountNumber, string expectedResult)>
+            {
+                (null, string.Empty),
+                (string.Empty, string.Empty),
+                (SIMPLE_ACCOUNT_NUMBER, string.Empty),
+                (accountNumber, BANK_CODE)
+            };
+
+            foreach (var config in testConfigs)
+            {
+                var accNumber = config.accountNumber;
+                var expected = config.expectedResult;
+                var information = new BankInformation { AccountNumber = accNumber };
+
+                var actual = information.GetBankCode();
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ InvoicingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InvoicingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InvoicingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7e67877-e251-4545-afe6-fc4fba8eb4a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
